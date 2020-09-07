6a347b75396bad2febe0909b9121f2923ee55bd7
cristianiniguez <felizpc1@gmail.com>
2020-09-07 16:20:16 -0400

    Iniciando con el servicio de firestore

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ signinForm.addEventListener('submit', e => {
   auth
     .signInWithEmailAndPassword(email, password)
     .then(userCredentials => {
-      signupForm.reset()
-      $('#signup-modal').modal('hide')
+      signinForm.reset()
+      $('#signin-modal').modal('hide')
       console.log('sign in')
     })
 })
@@ -40,4 +40,39 @@ logout.addEventListener('click', e => {
   auth.signOut().then(() => {
     console.log('sign out')
   })
+})
+
+// Posts
+const postsList = document.querySelector('#posts')
+const setupPosts = data => {
+  if (data) {
+    let html = ''
+    data.forEach(doc => {
+      const post = doc.data()
+      const li = `
+        <li class="list-group-item list-group-item-action">
+          <h5>${post.title}</h5>
+          <p>${post.description}</p>
+        </li>
+      `
+      html += li
+    })
+    postsList.innerHTML = html
+  } else {
+    postsList.innerHTML = '<p class="text-center">Login to see Posts</p>'
+  }
+}
+
+// Events
+// list for auth state changes
+auth.onAuthStateChanged(user => {
+  if (user) {
+    fs.collection('posts')
+      .get()
+      .then(snapshot => {
+        setupPosts(snapshot.docs)
+      })
+  } else {
+    setupPosts()
+  }
 })
